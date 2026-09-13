4ebc5e9fe7c62142bab6335763ac0f8913be3b40
rowanvignali <rowan.vignali@gmail.com>
2026-09-12 20:00:19 -0400

    Added scrolling to projects and added cookies for README and project lists (so that I stop getting rate limited lol)

--- static.js
+++ static.js
@@ -1,3 +1,23 @@
-export async function request(url, headers) {
+export async function request(url, alias, headers) {
+	let cookies = decodeURIComponent(document.cookie).split(";")
+	
+	for (let index = 0; index < cookies.length; index++) {
+		let cookie = cookies[index]
+		
+		while (cookie.charAt(0) == ' ') {
+			cookie = cookie.substring(1)
+		}
+
+		if (cookie.indexOf(alias + "=") == 0) {
+			return JSON.parse(cookie.substring(alias.length + 1, cookie.length))
+		}
+	}
+
 	return await fetch(url, {headers: headers}).then(response => response.json())
+}
+
+export function saveRequest(alias, data) {
+	const date = new Date()
+	date.setTime(date.getTime() + 60 * 60 * 1000)
+	document.cookie = alias + "=" + JSON.stringify(data) + ";" + "expires=" + date.toUTCString() + ";path=/"
 }
